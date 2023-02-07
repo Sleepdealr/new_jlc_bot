b53707e54956837be18ab2c24b75a6b2d276326b
Sleepdealer <35879391+Sleepdealr@users.noreply.github.com>
2023-02-07 17:52:24 -0500

    Add start script

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Pulls from git
+# Sometimes it doesn't feel like pulling so you need to stash and reset it
+# There is probably a better way of doing this
+# Comment out to disable
+git stash
+git stash drop
+git pull
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+
+python3 app.py
